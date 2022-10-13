089e144785c18d4e31bbd66dd067339616f54cad
Nadbelyh <nadbelyh@yandex.ru>
2022-10-13 21:01:11 +0500

    Домашнее задание к лекции 4 «Объекты»

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  {
+    name, gender, age;
+  }
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+
+  for (let mark of marks) {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  return  this.marks.reduce(function(sum, elem) {
+    return sum + elem;
+  }, 0) / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
