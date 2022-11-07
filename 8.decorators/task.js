119819b46108d3d9078ca69ec99fb5ab741132f4
Nadya <nadbelyh@yandex.ru>
2022-11-07 13:38:31 +0500

    Домашнее задание к лекции. «8. Декораторы»

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,45 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log(`Из кэша: ${objectInCache.value}`);
+      return `Из кэша: ${objectInCache.value}`;
+    }
+
+    let result = func(...args);
+    cache.push({ hash: hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log(`Вычисляем: ${result}`);
+    return `Вычисляем: ${result}`;
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timeoutId);
+  }
+
+  timeoutId = setTimeout(() => {
+    wrapper.count++;
+    func(...args);
+  }, delay);
+
+  return wrapper;
 }
